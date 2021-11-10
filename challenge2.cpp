4dc61f57ac85954839dddb04e18bd5c253e1e13a
souayrioss <souayri.oussama@gmail.com>
2021-11-10 13:31:17 +0100

    challenge2

--- challenge2.cpp
+++ challenge2.cpp
@@ -4,9 +4,11 @@
 int main(int argc, char *argv[])
 {
 	float C,F;
+	const int nb1=32,nb2=(5/9);
+
 	printf("enter la temperature en Fahrenheit ");
 	scanf("%f",&F);
-	C=(F-32)*5/9;
+	C=(F-nb1)* nb2;
 	printf("la temperature en degre Celsius %f ",C);
 	return 0;
 }
