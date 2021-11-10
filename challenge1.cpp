b1058014bf2c9c71b42aa93778d5b8ed3ad91b2c
souayrioss <souayri.oussama@gmail.com>
2021-11-10 11:45:06 +0100

    challenge1

---
+++ challenge1.cpp
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+int main(int argc, char *argv[])
+{
+	char nom[]="0";
+	char prenom[]="0";
+	int age;
+	char sexe[]="0";
+	char numTele[]="0";
+	printf("Entrer votre nom :");
+	scanf("%s",nom);
+	printf("Entrer votre prenom :");
+	scanf("%s",prenom);
+	printf("Entrer votre age :");
+	scanf("%d", &age);
+	printf("Entrer votre sexe :");
+	scanf("%s", &sexe);
+	printf("Entrer votre numero de telephone :");
+	scanf("%s", &numTele);
+	printf("voila votre donnees %s,%s,%d,%s,%s " ,nom,prenom,age,sexe,numTele);
+	return 0;23
+}
